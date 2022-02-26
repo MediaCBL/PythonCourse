ea79801fe7e76d48668b4766700d7a741c204b99
Media CBL <78509944+musiquecbl@users.noreply.github.com>
2022-02-26 08:39:18 -0500

    Test file open

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -13,3 +13,21 @@ print(f"{addedlist[0]}")
 #Dictionnary nesting
 MyDict = {1 : [1,2,3], 2 : [4,5,6], 3 : [7,8,9]}
 print(MyDict[1][1])
+
+#Test file open function
+MyFile = open("TextTest.txt")
+
+#print(MyFile.read())
+
+print(MyFile.readlines())
+
+#close to file to avoid error
+MyFile.close()
+
+with open("sfdsdfsdf.txt", mode="w") as f:
+    f.write("I created this file")
+
+
+#Print working directory only work in notebook
+#print(pwd)
+
