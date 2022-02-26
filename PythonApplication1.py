a13d6251221754148e31a485f7e11594f03716c6
Media CBL <78509944+musiquecbl@users.noreply.github.com>
2022-02-25 21:17:51 -0500

    First python test in VS outside of anaconda jupyter notebook

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,15 @@
-print("Hello World")
+#test variable insertion into strings
+test_string = "test"
+print(f'I want to {test_string}')
+
+#test add elements to list
+mylist = [1,2,3]
+mylist
+
+newlist = [4,5,6]
+addedlist = mylist+newlist
+print(f"{addedlist[0]}")
+
+#Dictionnary nesting
+MyDict = {1 : [1,2,3], 2 : [4,5,6], 3 : [7,8,9]}
+print(MyDict[1][1])
